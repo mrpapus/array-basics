e83fbf4652e602a3604c7d0acca285081fe52037
mrpapus <124309793+mrpapus@users.noreply.github.com>
2023-02-22 14:34:58 -0700

    Update main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+// array basics
+//array = an ordered collection of data
+let evenNums = [2, 4, 6, 8, 10, 12];
+let vowels = ["a", "e", "i", "o", "u"];
+let emptyArray = [];
+
+// access elements
+let Nums = [2, 4, 6, 8, 10, 12];
+//index
