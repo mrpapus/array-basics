3a21669d17aeb559b2dbfbe15d3cb64f744781d4
mrpapus <124309793+mrpapus@users.noreply.github.com>
2023-02-27 14:27:42 -0700

    Update main.js

--- main.js
+++ main.js
@@ -8,3 +8,10 @@ let emptyArray = [];
 let evenNums = [2, 4, 6, 8, 10, 12];
 //index
 console.log(vowels[2]);
+
+emptyArray.push("hi");
+console.log(emptyArray);
+emptyArray.push("hello");
+console.log(emptyArray);
+emptyArray.push("hey");
+console.log(emptyArray);
