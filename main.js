3d440148d6bf9336a883532f7c825f7dd4bc607e
mrpapus <124309793+mrpapus@users.noreply.github.com>
2023-03-01 14:38:37 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,4 @@
-// array basics
-//array = an ordered collection of data
-//let evenNums = [2, 4, 6, 8, 10, 12];
-let vowels = ["a", "e", "i", "o", "u"];
-let emptyArray = [];
-
-// access elements
-let evenNums = [2, 4, 6, 8, 10, 12];
-//index
-console.log(vowels[2]);
-
-emptyArray.push("hi");
-console.log(emptyArray);
-emptyArray.push("hello");
-console.log(emptyArray);
-emptyArray.push("hey");
-console.log(emptyArray);
+let happyArray = [];
+for (let n = 1; n <= 100; n++) {
+  happyArray.push("happy");
+}
