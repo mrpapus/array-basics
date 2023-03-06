d8c7b3529809fed89cc683c94973a42ea2c1b328
mrpapus <124309793+mrpapus@users.noreply.github.com>
2023-03-06 14:23:32 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
-let happyArray = [];
-for (let n = 1; n <= 100; n++) {
-  happyArray.push("happy");
+function randomDec(low, high) {
+  return Math.random() * (high - low) + low;
 }
+
+function randomInt(low, high) {
+  return Math.floor(randomDec(low, high));
+}
+
+// let happyArray = [];
+// for (let n = 50; n <= 200; n += 20) {
+//   happyArray.push(n);
+// }
+
+let myStr = "ford,chev,kia,toyota,honda";
+let myArray = myStr.split(",");
+console.log(myArray);
