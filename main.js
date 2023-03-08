6a7d1c6313f3ae572fb005154f436e67ea275e90
mrpapus <124309793+mrpapus@users.noreply.github.com>
2023-03-08 14:11:13 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
-for (let n = 1; n <= 20; n++) {
-  console.log(randomDec(1, 6));
+let names = [
+  "rita",
+  "cole",
+  "nate",
+  "ethan",
+  "adam",
+  "maklin",
+  "isaac",
+  "kyan",
+  "eddie",
+];
+
+let outputEl = document.getElementById("output");
+
+for (let n = 0; n < names.length; n++) {
+  outputEl.innerHTML += `<p>hello ${names[n]}!</P>`;
 }
