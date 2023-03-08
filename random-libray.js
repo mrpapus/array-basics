a6a646c7c0e80d980c1d0f97e115132a0e1fa7da
mrpapus <124309793+mrpapus@users.noreply.github.com>
2023-03-08 14:11:07 -0700

    Update random-libray.js

--- random-libray.js
+++ random-libray.js
@@ -1,3 +1,14 @@
 function randomDec(low, high) {
   return Math.random() * (high - low) + low;
 }
+//random integer
+function randomInt(low, high) {
+  return Math.floor(Math.random() * (high - low) + low);
+}
+
+function randomRGB() {
+  return `rgb(${randomInt(0, 256)}, ${randomInt(0, 256)}, ${randomInt(
+    0,
+    256
+  )})`;
+}
